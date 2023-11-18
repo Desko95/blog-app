1bc7d5c4dfc0a08c624c21a1f5842becc07335db
Desko <simonedescode@gmail.com>
2023-11-18 15:50:43 +0000

    Hero section fixed

--- app/home/HeroSection.tsx
+++ app/home/HeroSection.tsx
@@ -5,8 +5,19 @@ type Props = {};
 
 const HeroSection = (props: Props) => {
   return (
-    <section>
-      <Image src="/coffee.png" width={300} height={300} alt="person walking" />
+    <section className="grid grid-cols-1 sm:grid-cols-12">
+      <div className="col-span-7">
+        <h1 className="text-2xl sm:text-4xl">Tech News for Busy Peeps</h1>
+        <button>Browse Articles</button>
+      </div>
+      <div className="col-span-5">
+        <Image
+          src="/coffee.png"
+          width={300}
+          height={300}
+          alt="person walking"
+        />
+      </div>
     </section>
   );
 };
