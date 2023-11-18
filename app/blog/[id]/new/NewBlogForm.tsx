aa4af3aa787171f93d7b703e3506ae83ac4b658f
Desko <simonedescode@gmail.com>
2023-11-18 21:57:49 +0000

    client and server components

---
+++ app/blog/[id]/new/NewBlogForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const NewBlogForm = (props: Props) => {
+  return (
+    <div>NewBlogForm</div>
+  )
+}
+
+export default NewBlogForm;
---
+++ app/blog/[id]/new/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NewBlogForm from './NewBlogForm'
+
+
+type Props = {}
+
+const page = async (props: Props) => {
+  return (
+    <div>
+      <NewBlogForm  />
+    </div>
+  )
+}
+
+export default page;
