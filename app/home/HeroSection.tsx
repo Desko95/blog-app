a474c9cec6f73914e833f8df90b5b59cbe09acea
Desko <simonedescode@gmail.com>
2023-11-18 14:00:45 +0000

    me arrendo porcodio

---
+++ app/home/HeroSection.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from "next/image";
+
+type Props = {};
+
+const HeroSection = (props: Props) => {
+  return (
+    <section>
+      <Image src="/coffee.png" width={300} height={300} alt="person walking" />
+    </section>
+  );
+};
+
+export default HeroSection;
