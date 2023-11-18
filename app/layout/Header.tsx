ffc3fffe992d7e6d68d0c6cb9e555c0d107fe395
Desko <simonedescode@gmail.com>
2023-11-18 13:13:18 +0000

    .

--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -4,7 +4,10 @@ type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <div>Header</div>
+    <div className='border-b flex justify-between'>
+        <div className='text-4xl px-2 py-4'>LOGO</div>
+        <button className='text-white bg-black px-4 py-4'>Sign In</button>
+    </div>
   )
 }
 
