79c5c57cbab7c9513d7a5480de0ce68a6a4cc30d
Desko <simonedescode@gmail.com>
2023-11-18 17:37:53 +0000

    created postcard e posts tsx

--- app/home/Posts.tsx
+++ app/home/Posts.tsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import { PrismaClient } from '@prisma/client'
-
+import PostCard from './PostCard'
 
 const prisma = new PrismaClient()
 
 type Props = {}
 
 const Posts = async (props: Props) => {
-  const users = await prisma.user.findMany()
-  console.log(users)
-  const posts = await prisma.user.findMany()
-  console.log(posts);
+  const posts = await prisma.post.findMany({
+    take: 3,
+    orderBy: {
+    },
+  },
+  )
+
+  const bgClasses = [
+    'bg-pink-500',
+    'bg-blue-500',
+    'bg-yellow-500',
+  ]
+
   return (
-    <div>Posts</div>
+    <div><h2 className='text-4xl text-center mt-6'>Trending</h2>
+      <div className='grid grid-cols-1 sm:grid-cols-3 gap-4 mt-4'>
+        {posts.map((post, index) => (
+          <PostCard key={post.id} post={post} className={bgClasses[index]} />
+        ))}
+      </div>
+    </div>
   )
 }
 
